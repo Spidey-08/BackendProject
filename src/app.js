f64644a64357fdccc1f04f648550f1079fcbf127
Spidey-08 <jaduu14@gmail.com>
2026-02-18 23:38:48 +0530

    Updated project with new features added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express()
 
@@ -7,6 +8,8 @@ const app = express()
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public")); 
+
+app.use(cookieParser())
 
 //Cors Configuration
 app.use(cors({
@@ -19,8 +22,10 @@ app.use(cors({
 
 //Imports the Routes
 import healthCheckRoutes from "./routes/healthcheck.routes.js";
+import authRouter from "./routes/auth.routes.js";
 
 app.use("/api/v1/healthcheck", healthCheckRoutes);
+app.use("/api/v1/auth", authRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
---
+++ src/validators/index.js
@@ -0,0 +1,29 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+    body("username")
+      .trim()
+      .notEmpty()
+      .withMessage("Username is required")
+      .isLowercase()
+      .withMessage("Username must be at least 3 Characters long"),
+    body("password").trim().notEmpty().withMessage("Password is required"),
+    body("fullName").optional().trim(),
+  ];
+};
+
+const userLoginValidator = () => {
+  return [
+    body("email").optional().isEmail().withMessage("Email is Invalid"),
+    body("password").notEmpty().withMessage("Password is required")
+  ];
+};
+
+export { userRegisterValidator, userLoginValidator };
