7855fc469b216fc3ffb827c5b4b0b5f73e452ea4
Spidey-08 <jaduu14@gmail.com>
2026-02-20 00:59:30 +0530

    added more routes feature

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,53 @@
 import { Router } from "express";
-import { login, logoutUser, registerUser } from "../controllers/auth.Controllers.js";
+import {
+  changeCurrentPassword,
+  forgotPasswordRequest,
+  getCurrentUser,
+  login,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  resendEmailVerification,
+  resetForgotPassword,
+  verifyEmail,
+} from "../controllers/auth.Controllers.js";
 import { validate } from "../middlewares/validator.middleware.js";
-import { userRegisterValidator, userLoginValidator} from "../validators/index.js";
+import {
+  userChangeCurrentPasswordValidator,
+  userForgotPasswordValidator,
+  userLoginValidator,
+  userRegisterValidator,
+  userResetForgotPasswordValidator,
+} from "../validators/index.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(userRegisterValidator(), validate, registerUser);
-router.route("/login").post(userLoginValidator(), validate, login);
-router.route("/logout").post(verifyJWT, logoutUser);
+// Unsecured Routes
+router.route("/register").post(userRegisterValidator(), validate, registerUser);    // work with email, password, username
+router.route("/login").post(userLoginValidator(), validate, login);     // work with email, password, username
+router.route("/verify-email/:verificationToken").get(verifyEmail);
+router.route("/refresh-token").post(refreshAccessToken);    // no needed
+router
+  .route("/forgot-password")    // work with email 
+  .post(userForgotPasswordValidator(), validate, forgotPasswordRequest);
+router
+  .route("/reset-password/:resetToken")
+  .post(userResetForgotPasswordValidator(), validate, resetForgotPassword);
+
+// Secure Routes
+router.route("/logout").post(verifyJWT, logoutUser);    // no needed
+router.route("/current-user").post(verifyJWT, getCurrentUser);  // no needed
+router
+  .route("/change-password")    // work with oldPassword and newPassword with exact this name
+  .post(
+    verifyJWT,
+    userChangeCurrentPasswordValidator(),
+    validate,
+    changeCurrentPassword,
+  );
+router
+  .route("/resend-email-verification")  // no needed
+  .post(verifyJWT, resendEmailVerification);
 
 export default router;
--- src/validators/index.js
+++ src/validators/index.js
@@ -22,8 +22,36 @@ const userRegisterValidator = () => {
 const userLoginValidator = () => {
   return [
     body("email").optional().isEmail().withMessage("Email is Invalid"),
-    body("password").notEmpty().withMessage("Password is required")
+    body("password").notEmpty().withMessage("Password is required"),
   ];
 };
 
-export { userRegisterValidator, userLoginValidator };
+const userChangeCurrentPasswordValidator = () => {
+  return [
+    body("oldPassword").notEmpty().withMessage("Old password is required"),
+    body("newPassword").notEmpty().withMessage("New password is required"),
+  ];
+};
+
+const userForgotPasswordValidator = () => {
+  return [
+    body("email")
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+  ];
+};
+
+
+const userResetForgotPasswordValidator = () => {
+  return [body("newPassword").notEmpty().withMessage("Password is required")];
+};
+
+export {
+  userRegisterValidator,
+  userLoginValidator,
+  userChangeCurrentPasswordValidator,
+  userForgotPasswordValidator,
+  userResetForgotPasswordValidator,
+};
